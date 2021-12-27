142b29e96c5039c59210861a80790d7336dd1e96
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2021-12-27 19:06:08 -0300

    feat: validation of fields

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  validates :name, :date_of_birth, :phone, :address, :credit_card, :franchise, :email, :user, presence: true
+  validates :name, format: { with: /\A[[:alnum:] -]+\z/ }
+  validates :credit_card, credit_card_number: {brands: [:amex, :diners, :discover, :jcb, :mastercard, :visa]}
 end
