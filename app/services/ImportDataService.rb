bb8c417671d2ab0dc432aa1253e39611418a9f65
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2021-12-27 19:04:38 -0300

    feat: create import data service

---
+++ app/services/ImportDataService.rb
@@ -0,0 +1,25 @@
+require 'csv'
+
+class ImportDataService
+    def initialize(file, current_user)
+      @file = file
+      @current_user = current_user
+    end
+
+    def call
+        CSV.foreach(@file.path, headers: true) do |row|
+            detector = CreditCardValidations::Detector.new(row[4])
+            franchise = detector.brand
+            Contact.create(
+              name: row[0],
+              date_of_birth: row[1],
+              phone: row[2],
+              address: row[3],
+              credit_card: row[4],
+              franchise: franchise,
+              email: row[5],
+              user: @current_user
+            )
+          end
+    end
+end
