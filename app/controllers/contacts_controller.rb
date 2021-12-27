bada8428376e80471e180ce5fac26a62b206f205
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2021-12-27 19:05:06 -0300

    feat: create logic in import

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,15 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
   end
 
   def import
+    ImportDataService.new(params[:file], current_user).call
+    redirect_to contacts_index_path, notice: 'imported'
+  end
+
+  def destroy
+    current_user.contacts.delete_all
+    redirect_to contacts_index_path, notice: 'deleted all contacts'
   end
 end
