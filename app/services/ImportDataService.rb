a086027f1870aafbdc24fd6d39c07ab9893bd685
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2021-12-29 11:02:40 -0300

    fix: adjustment in ImportDataService

--- app/services/ImportDataService.rb
+++ app/services/ImportDataService.rb
@@ -7,19 +7,23 @@ class ImportDataService
     end
 
     def call
-        CSV.foreach(@file.path, headers: true) do |row|
-            detector = CreditCardValidations::Detector.new(row[4])
-            franchise = detector.brand
-            Contact.create(
-              name: row[0],
-              date_of_birth: row[1],
-              phone: row[2],
-              address: row[3],
-              credit_card: row[4],
-              franchise: franchise,
-              email: row[5],
-              user: @current_user
-            )
-          end
+      CSV.foreach(@file.path, headers: true) do |row|
+          detector = CreditCardValidations::Detector.new(row[4])
+          franchise = detector.brand
+          save(row, franchise)
+      end
+    end
+
+    def save(row, franchise)
+      Contact.create(
+        name: row[0],
+        date_of_birth: row[1],
+        phone: row[2],
+        address: row[3],
+        credit_card: row[4],
+        franchise: franchise,
+        email: row[5],
+        user: @current_user
+      )
     end
 end
